76224f99c681acd9d2079cc758cd8680c24f7ed0
R09C <117595346+R09C@users.noreply.github.com>
2023-11-17 02:20:10 +0700

    Add files via upload

--- technical/find_interval.py
+++ technical/find_interval.py
@@ -1,7 +1,10 @@
 def find_intrerval(f, a0=-1000, b0=1000):
     mass = []
     while a0 < b0:
-        if f(a0) * f(a0 + 1) <= 0:
-            mass.append({"a": a0, "b": a0 + 1})
+        try:
+            if f(a0) * f(a0 + 1) <= 0:
+                mass.append({"a": a0, "b": a0 + 1})
+        except:
+            print("деление на ноль")
         a0 += 1
     return mass
